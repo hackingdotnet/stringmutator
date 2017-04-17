0cd9c1aeeb0fed9f770ff2ea8049953838a34aaa
Eddie Rangel <hacker@eddierangel.com>
2017-04-17 15:35:22 -0700

    Refactoring Tests. Adding features. Adding corresponding tests.

--- StringMutator.Extension/Mutator.cs
+++ StringMutator.Extension/Mutator.cs
@@ -28,5 +28,96 @@ namespace StringMutator.Extension
             return input.Split('\n');
         }
 
+        /// <summary>
+        /// Returns a string with the first character Capitalized
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Capitalize(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException();
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+
+        /// <summary>
+        /// Returns a String that has every word Capitalized
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string Titleize(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException();
+
+            var result = input.Split(' ');
+
+            for (int i = 0; i <= result.Length - 1; i++)
+            {
+                result[i] = Capitalize(result[i]);
+            }
+
+            return String.Join(" ", result);
+        }
+
+        /// <summary>
+        /// Returns a String Array with each word in an index
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string[] Words(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException();
+            return input.Split(' ');
+        }
+
+        /// <summary>
+        /// Returns a Char Array with each Char in an index
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static char[] Characters(this string input)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException();
+            return input.ToCharArray();
+        }
+
+        /// <summary>
+        /// Return a Chopped String as a String Array
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string[] Chop(this string input, int value)
+        {
+            if (String.IsNullOrEmpty(input))
+                throw new ArgumentNullException();
+
+            var inputLength = input.Length;
+            var size = inputLength / value;            
+            var remainder = inputLength % value;
+            var stop = size + 1;
+
+            string[] result = new string[size + remainder];
+            var move = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                result[i] = input.Substring(move, size);
+                move += size;
+            }
+
+            if (remainder != 0)
+            {
+                result[size] = input.Substring(move, remainder);
+            }
+            
+            return result;
+        }
+
+
+
     }
 }
---
+++ StringMutator.Tests/TestMutator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Xunit;
+using StringMutator.Extension;
+
+namespace StringMutator.Tests
+{
+    public class TestMutator
+    {
+        [Fact]
+        public void Can_Chop_Lines()
+        {
+            string welcome = "Welcome to the\nString Mutator.";
+
+            Assert.Equal(2, welcome.Lines().Length);
+        }
+
+        [Fact]
+        public void Can_Capitalize_First_Word()
+        {
+            // Arrange
+
+            string welcome = "welcome to the string mutator.";
+            string expected = "Welcome to the string mutator.";
+
+            // Act
+            var actual = welcome.Capitalize();
+
+            // Assert
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Fact]
+        public void Can_Return_Titleized_String()
+        {            
+            //Arrange
+            var primer = "lorem ipsum dolor";
+            var expected = "Lorem Ipsum Dolor";
+            //Act
+            var actual = primer.Titleize();
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Can_Return_Words()
+        {
+            //Arrange
+            var primer = "lorem ipsum dolor";
+            var expected = new string[] { "lorem", "ipsum", "dolor" };
+            //Act
+            var actual = primer.Words();
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Can_Return_Characters()
+        {
+            //Arrange
+            var primer = "lorem";
+            var expected = new char[] { 'l', 'o', 'r', 'e', 'm' };
+            //Act
+            var actual = primer.Characters();
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+    
+        [Fact]
+        public void Can_Return_Chop()
+        {
+            //Arrange
+            var primer = "lorem ipsum dolor"; // 17 char's
+            var expected = new string[] { "lore", "m ip", "sum ", "dolo", "r" };
+            //Act
+            var actual = primer.Chop(4); // chop into 4 chars array elements
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
